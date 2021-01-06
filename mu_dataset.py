c424736b9bdfc2a5a18dc9eba1f3af95d1e1595a
aerjayc <50480123+aerjayc@users.noreply.github.com>
2021-01-06 23:14:39 +0800

    Add new features to function
    `make_dataset` now allows exporting to a file (as a `.csv`), an
    adjustable delay every iteration, and option to select particular lists
    only.
    It also prints some info to show what it is doing while iterating.
    It returns a value only if no `filename` is given.

--- mu_dataset.py
+++ mu_dataset.py
@@ -1,16 +1,40 @@
 from mangaupdates import public
+import csv
 import pandas as pd
+import time
 
-def make_dataset(series_ids):
 
-    rows = []
+def make_dataset(series_ids, filename=None, delay=10, list_names=None):
+
+    col_names = ('userid', 'username', 'score', 'listname', 'seriesid')
+    if filename is not None:
+        f = open(filename, 'a', newline='')
+        writer = csv.writer(f)
+        writer.writerows([col_names])
+
+    if filename is None:
+        rows = []
+
+    if list_names is None:
+        list_names = ('read', 'wish', 'unfinished')
+    print('Lists:', list_names)
+
     for sid in series_ids:
         lists = public.ListStats(sid)
-        lists.populate()
+        print(sid, end='\t')
+        lists.populate(list_names=list_names)
+        time.sleep(delay)
 
-        for key in ('read', 'wish', 'unfinished', 'custom'):
-            rows.extend([(*row, key) for row in lists.general_list(key)])
+        for key in list_names:
+            new_rows = [(*val, key, sid) for val in lists.general_list(key)]
+            print(f'{key}:', len(new_rows), 'rows.')
+            if filename is None:
+                rows.extend(new_rows)
+            else:
+                writer.writerows(new_rows)
 
-    dataset = pd.DataFrame(rows)
-
-    return dataset
+    if filename is not None:
+        f.close()
+        return
+    else:
+        return rows
