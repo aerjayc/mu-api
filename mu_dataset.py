b5eaf1a6b6af02a2ab7b586acaec629c21c5af3d
aerjayc <50480123+aerjayc@users.noreply.github.com>
2021-01-07 23:47:33 +0800

    Add main function, to be used as script

--- mu_dataset.py
+++ mu_dataset.py
@@ -38,3 +38,30 @@ def make_dataset(series_ids, filename=None, delay=10, list_names=None):
         return
     else:
         return rows
+
+
+if __name__ == '__main__':
+    lists_fname = 'lists.csv'
+    dataset_fname = 'the_dataset.csv'
+    list_names = ['read']
+    headers = False
+    N = 10
+
+    # Get unique series IDs from file
+    series_ids = []
+    with open(lists_fname, newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        if headers:
+            header_names = next(csvreader)
+            sid_index = header_names.index('seriesid')
+        else:
+            sid_index = 0
+
+        for i,row in enumerate(csvreader):
+            if i >= N:
+                break
+            sid = int(row[sid_index])
+            if sid not in series_ids:
+                series_ids.append(sid)
+    print(series_ids)
+    make_dataset(series_ids, filename=dataset_fname, delay=10, list_names=list_names)
